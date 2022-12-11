81bfbd2bf906fd6b3762bbe4cde705226440ee44
waveofmymind <sjun0913@gmail.com>
2022-12-12 00:06:36 +0900

    [#5] 테스트 컨트롤러 작성, Oauth 로그인 구현하기

---
+++ src/main/java/juun/ars/controller/TestController.java
@@ -0,0 +1,29 @@
+package juun.ars.controller;
+
+import juun.ars.domain.Member;
+import juun.ars.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class TestController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/join")
+    public String addMember() {
+        return "/member/memberJoinForm";
+    }
+
+    @PostMapping("/join")
+    public String createMember(@ModelAttribute Member member) {
+        memberRepository.save(member);
+        return "/member/memberResult";
+    }
+}
