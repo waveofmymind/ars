29786e7bd8a3d72dfd50fe4b36145a3b9d708a55
waveofmymind <sjun0913@gmail.com>
2022-12-14 21:09:11 +0900

    [#1] 다시 새롭게

--- src/main/java/juun/ars/domain/Member.java
+++ src/main/java/juun/ars/domain/Member.java
@@ -1,48 +1,24 @@
 package juun.ars.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name = "member")
+@Table(name="member")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member extends BaseTimeEntity implements Serializable {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
     private Long id;
 
-    @Column
     private String username;
-    @Column
-    private String password;
-    @Column
+
     private String email;
-    private String provider;
-    private String providerId;
-
-
-    private String role;
-
-    @Builder
-    public Member(String username, String password, String email, String role, String provider, String providerId) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.role = role;
-        this.provider = provider;
-        this.providerId = providerId;
-    }
-
-    public Member update(String username){
-        this.username = username;
-        return this;
-    }
 
 }
--- src/main/java/juun/ars/domain/Problem.java
+++ src/main/java/juun/ars/domain/Problem.java
@@ -1,50 +1,28 @@
 package juun.ars.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
-import static java.util.Collections.singletonList;
-
+@Table(name="problem")
 @Entity
 @Getter
-@Setter
-@Table(name = "problem")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Builder
-public class Problem extends BaseTimeEntity {
+@NoArgsConstructor
+public class Problem {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "problem_id")
+    @Id @GeneratedValue
     private Long id;
 
+    private Long title;
+
     @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member writer; // 해결한 사람
+    @JoinColumn(name="member_id")
+    private Member writer;
 
-    private String title; // 문제 이름
-
-    private String link; // 문제 링크
+    private String link;
 
     private int step;
-
-    @Builder.Default
-    @OrderBy("createdDate desc")
-    @OneToMany(mappedBy = "problem", fetch = FetchType.LAZY,cascade = CascadeType.ALL)
-    private List<Review> reviewList = new ArrayList<>();
-
-    public void setReview(Review review) {
-        this.reviewList = singletonList(review);
-    }
-
-    public void updateStep(int step) {
-        this.step = step;
-    }
-
-
-
-
 }
