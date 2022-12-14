57ac10f691129a749537148aa8e4b0d073871257
waveofmymind <sjun0913@gmail.com>
2022-12-14 21:07:57 +0900

    [#1] 다시 새롭게

--- src/main/java/juun/ars/config/auth/provider/OAuth2UserInfo.java
+++
@@ -1,8 +0,0 @@
-package juun.ars.config.auth.provider;
-
-public interface OAuth2UserInfo {
-    String getProviderId();
-    String getProvider();
-    String getEmail();
-    String getName();
-}
--- src/main/java/juun/ars/utils/MemberContext.java
+++
@@ -1,7 +0,0 @@
-package juun.ars.utils;
-
-import juun.ars.domain.Member;
-
-public class MemberContext {
-    public static ThreadLocal<Member> currentMember = new ThreadLocal<>();
-}
