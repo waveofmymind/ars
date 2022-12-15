030e847a69b6044a7aa00d4d17af62823f7c2592
waveofmymind <sjun0913@gmail.com>
2022-12-15 22:56:37 +0900

    [#3] 홈 컨트롤러 회원가입, 가입 결과까지 구현

--- src/main/java/juun/ars/controller/HomeController.java
+++ src/main/java/juun/ars/controller/HomeController.java
@@ -1,16 +1,36 @@
 package juun.ars.controller;
 
+import juun.ars.domain.Member;
+import juun.ars.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final MemberService memberService;
 
     @RequestMapping("/")
     public String home() {
         log.info("home controller");
         return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String add() {
+        return "/member/memberJoinForm";
+    }
+
+    @PostMapping("/members/new")
+    public String addMember(@ModelAttribute Member member) {
+        memberService.joinMember(member);
+        return "/member/memberResult";
     }
 }
